cbc6dfd09955a561691b5140d0d338f9689d1ec4
Batel Haiylo <inpo686@gmail.com>
2022-12-12 21:26:25 +0200

    .

---
+++ src/router/Router.component.jsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import {  Home, Dashboard, PageError } from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProductsProvider from "../context/ProductsProvider.component";
+import OrdersProvider from "../context/OrdersProvider.component";
+import UsersProvider from "../context/UsersProvider.component";
+
+export default function Router() {
+  return (
+    <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="dashboard"
+          element={
+            <ProductsProvider>
+              <OrdersProvider>
+              <UsersProvider>
+                <Dashboard />
+            </UsersProvider>
+              </OrdersProvider>
+            </ProductsProvider>
+          }
+        />
+
+        <Route path="*" element={<PageError />} />
+    </Routes>
+  );
+}
+      // <Route
+      //   path="/"
+      //   element={<UserAccsessPage />} />
